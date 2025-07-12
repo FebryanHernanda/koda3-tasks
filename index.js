c25cc7c46b5117a75022d2eb7e2504e7af3c7d70
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-07-12 13:11:51 +0700

    fix(main): return the code that i dont use as it was before and fix some error handling

--- index.js
+++ index.js
@@ -4,43 +4,41 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul() {
+function tasksIrul(){
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10, 50);
+    const resultSum = irul.sumNumber(10,50);
     console.log(resultSum);
 
     // Failed
-    try {
-        const result = irul.sumNumber("10", 30);
+    try{
+        const result = irul.sumNumber("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10, 50);
+    const resultMultiply = irul.multiplyNumbers(10,50);
     console.log(resultMultiply);
 
     // Failed
-    try {
-        const result = irul.multiplyNumbers("10", 30);
+    try{
+        const result = irul.multiplyNumbers("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(
-        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
-    );
+    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
 
     // Failed
-    try {
-        const result = irul.celciusToFahrenheit("10", 30);
+    try{
+        const result = irul.celciusToFahrenheit("10",30);
         console.log(result);
-    } catch (error) {
+    }catch(error){
         console.log(error.message);
     }
 }
@@ -66,11 +64,7 @@ function taskFebryan() {
     
     /* Soal 2 - count array */
     console.log("----------Count Array----------");
-    try {
-        febryan.task2([1, 2, 3, 4, 5]);
-    } catch (error) {
-        console.log(error.message);
-    }
+    febryan.task2([1, 2, 3, 4, 5]);
     
     /* Soal 3 - reverse triangle */
     console.log("--------Triangle Ver. 2--------");
@@ -82,11 +76,7 @@ function taskFebryan() {
     
     /* Soal 4 - process numbers*/
     console.log("--------Process Numbers--------");
-    try {
-        febryan.task4([2, 4, 6, 8,]);
-    } catch (error) {
-        console.log(error.message);
-    }
+    febryan.task4([2, 4, 6, 8,]);
     
     /* Soal 5 - cetak table */
     console.log("----------Cetak Tabel----------");
@@ -105,11 +95,7 @@ function taskFebryan() {
     }
     /* Soal 7 - identifikasi error */
     console.log("-------Identifikasi Error--------");
-    try {
-        febryan.task7();
-    } catch (error) {
-        console.log(error.message);
-    }
+    febryan.task7();
 }
 
 taskFebryan();
