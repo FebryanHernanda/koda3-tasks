310ff2a55badc7c87ee1a0259b14af2702c45b05
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-07-12 11:27:57 +0700

    feat(main): add code for call all function task from febryan folder

--- index.js
+++ index.js
@@ -4,48 +4,80 @@
 
 const irul = require("./tasks/irul");
 
-function tasksIrul(){
+function tasksIrul() {
     // Program: Sum Number
     // Success
-    const resultSum = irul.sumNumber(10,50);
+    const resultSum = irul.sumNumber(10, 50);
     console.log(resultSum);
 
     // Failed
-    try{
-        const result = irul.sumNumber("10",30);
+    try {
+        const result = irul.sumNumber("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Multiply Number
     // Success
-    const resultMultiply = irul.multiplyNumbers(10,50);
+    const resultMultiply = irul.multiplyNumbers(10, 50);
     console.log(resultMultiply);
 
     // Failed
-    try{
-        const result = irul.multiplyNumbers("10",30);
+    try {
+        const result = irul.multiplyNumbers("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
     // Program: Convert Celcius to Fahrenheit
     // Success
     const temperatureResult = irul.celciusToFahrenheit(10);
-    console.log(`the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`);
+    console.log(
+        `the temperature ${temperatureResult.celcius}°C is ${temperatureResult.fahrenheit}°F`
+    );
 
     // Failed
-    try{
-        const result = irul.celciusToFahrenheit("10",30);
+    try {
+        const result = irul.celciusToFahrenheit("10", 30);
         console.log(result);
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
 }
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task Febryan
  */
 
 // import your tasks here
+
+const febryan = require("./tasks/febryan");
+
+/* Soal 1 - triangle */
+console.log("--------Triangle Ver. 1--------");
+febryan.task1(5);
+
+/* Soal 2 - count array */
+console.log("----------Count Array----------");
+febryan.task2([1, 2, 3, 4, 5]);
+
+/* Soal 3 - reverse triangle */
+console.log("--------Triangle Ver. 2--------");
+febryan.task3(5);
+
+/* Soal 4 - process numbers*/
+console.log("--------Process Numbers--------");
+febryan.task4([2, 4, 6, 8]);
+
+/* Soal 5 - cetak table */
+console.log("----------Cetak Tabel----------");
+febryan.task5(3);
+
+/* Soal 6 - buat rentang */
+console.log("----------Cetak Rentang----------");
+febryan.task6(5,10);
+
+/* Soal 7 - identifikasi error */
+console.log("-------Identifikasi Error--------");
+febryan.task7();
