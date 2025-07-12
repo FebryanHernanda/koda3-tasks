0175c6c0ac0af703ef2e7623b52b980a58099fb3
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-07-12 11:20:44 +0700

    feat(cetakTabel): add cetakTabel function feature

---
+++ tasks/febryan/cetakTable.js
@@ -0,0 +1,16 @@
+const cetakTabel = (n) => {
+    let result;
+
+    if (typeof n !== "number" || n < 0) {
+        const alert =
+      "Parameter harus bertipe data nomor dan harus nomor positif !";
+        return console.log(alert);
+    }
+
+    for (let i = 1; i <= 10; i++) {
+        result = n * i;
+        console.log(`${n} x ${i} = ${result}`);
+    }
+};
+
+module.exports = cetakTabel;
