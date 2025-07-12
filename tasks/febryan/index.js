38b5c95a809ec62110672545fac539aac34b3b6e
Febryan Hernanda <febryanhernanda@Febryans-MacBook-Pro.local>
2025-07-12 11:25:31 +0700

    feat(febryan-path): add index.js for export all function features from febryan folder

---
+++ tasks/febryan/index.js
@@ -0,0 +1,17 @@
+const task1 = require("./triangle");
+const task2 = require("./countArray");
+const task3 = require("./reverseTriangle");
+const task4 = require("./processNumbers");
+const task5 = require("./cetakTable");
+const task6 = require("./buatRentang");
+const task7 = require("./errorIdentification");
+
+module.exports = {
+    task1,
+    task2,
+    task3,
+    task4,
+    task5,
+    task6,
+    task7
+};
